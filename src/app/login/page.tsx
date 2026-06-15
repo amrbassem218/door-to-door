e7be7a6381cd04e6de4409d0c63ef881a7f16337
Amr Bassem ElTaweel <amrbeducation@gmail.com>
2026-06-15 22:03:13 +0300

    changed the sign in/up UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,20 @@
 'use client';
-interface ILoginProps {}
 import * as React from "react";
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import LoginCard from "./loginCard";
+import { LoginForm } from "@/components/login-form";
 
 function LoginContent() {
   const searchParams = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") || "/";
   return (
     <div className="flex w-full flex-1 items-center justify-center my-10">
-      <LoginCard redirectTo={redirectTo}/>
+      <LoginForm redirectTo={redirectTo} />
     </div>
   );
 }
 
-const Login: React.FunctionComponent<ILoginProps> = (props) => {
+const Login: React.FunctionComponent = () => {
   return (
     <Suspense fallback={<div className="flex w-full flex-1 items-center justify-center my-10"><p>Loading...</p></div>}>
       <LoginContent />
