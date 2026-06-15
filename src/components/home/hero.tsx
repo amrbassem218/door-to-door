46c99bc02bec77ef8b3a475d7468040a84482097
Amr Bassem ElTaweel <amrbeducation@gmail.com>
2026-06-15 22:25:52 +0300

    updated the hero

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
@@ -14,7 +16,7 @@ const Hero = () => {
       <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/70 to-transparent"></div>
       <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/0 to-transparent"></div>
 
-      <div className="absolute inset-0 z-10 flex items-center justify-center px-5">
+      <div className="absolute inset-0 z-10 flex items-center justify-center px-5 flex-col gap-5">
         <h1 className="text-5xl lg:text-8xl font-bold text-white text-center leading-none flex flex-col">
           <span className="">
             Local <span className="">the</span>{" "}
@@ -23,7 +25,7 @@ const Hero = () => {
           <span>
             <span className="relative inline-block">
               <span
-                className="absolute inset-0 -left-2 -right-2 -top-1 -bottom-1 bg-yellow-300/80 -rotate-7 rounded-sm"
+                className="absolute inset-0 -left-2 -right-2 -top-1 -bottom-1 bg-secondary -rotate-7 rounded-sm"
                 aria-hidden="true"
               />
               <span className="relative">right</span>
@@ -31,6 +33,13 @@ const Hero = () => {
             <span className="">way</span>
           </span>
         </h1>
+        <p className="text-primary-foreground opacity-70 text-center">
+          We're launching the first online marketplace for local brands here in
+          Egypt.
+        </p>
+        <Link href="/search">
+          <Button className="w-40 h-10">Shop All</Button>
+        </Link>
       </div>
     </div>
   );
