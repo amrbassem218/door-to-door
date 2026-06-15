af1e2de63edbb1b4707bd6b39fafadcbaac1f993
Amr Bassem ElTaweel <amrbeducation@gmail.com>
2026-06-15 19:12:48 +0300

    changed the hero

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,48 +1,36 @@
-import SearchBar from "@/components/ui/searchBar";
-import Link from "next/link";
-import * as React from "react";
-interface IHeroProps {}
+import Image from "next/image";
 
-const Hero: React.FunctionComponent<IHeroProps> = (props) => {
-  const suggestedKeywords = ["tomatoes", "olives", "lettuce", "onion"];
+const Hero = () => {
   return (
-    <div className="">
-      <div className="relative sm:h-150 h-100 overflow-hidden">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="absolute inset-0 w-full h-full object-cover"
-        >
-          <source src="/door2door_vid.mp4" type="video/mp4" />
-        </video>
+    <div className="relative sm:h-150 h-100 overflow-hidden">
+      <Image
+        src={"/hero.webp"}
+        alt="hero image"
+        fill
+        className="object-cover"
+        priority
+      />
 
-        {/* Gradient overlay - darker at bottom */}
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/70 to-transparent"></div>
-        <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/0 to-transparent"></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/70 to-transparent"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/0 to-transparent"></div>
 
-        <div className=" z-10 flex items-center justify-center lg:mx-20 ">
-          <div className="absolute left-0 sm:bottom-35 bottom-10 w-150 max-w-full px-5">
-            <h1 className="lg:text-5xl text-2xl font-bold mb-4 text-white ">
-              From Egyptian Fields to the World's Markets
-            </h1>
-            <div className="space-y-3">
-              <SearchBar styles="w-full h-12" />
-              <div className="flex gap-2">
-                {suggestedKeywords.map((word) => (
-                  <Link
-                    key={word}
-                    className="p-3 h-5 border-1 border-white flex items-center justify-center rounded-md text-white hover:bg-background hover:text-black cursor-pointer transition-all"
-                    href={`/search?query=${word}`}
-                  >
-                    {word}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="absolute inset-0 z-10 flex items-center justify-center px-5">
+        <h1 className="text-5xl lg:text-8xl font-bold text-white text-center leading-none flex flex-col">
+          <span className="">
+            Local <span className="">the</span>{" "}
+          </span>
+
+          <span>
+            <span className="relative inline-block">
+              <span
+                className="absolute inset-0 -left-2 -right-2 -top-1 -bottom-1 bg-yellow-300/80 -rotate-7 rounded-sm"
+                aria-hidden="true"
+              />
+              <span className="relative">right</span>
+            </span>{" "}
+            <span className="">way</span>
+          </span>
+        </h1>
       </div>
     </div>
   );
